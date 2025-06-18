eb55ac81f88889c4d4b4cc2da8c3524ebb26b827
Rahul Kumar <rahul.kumar@completrix.com>
2025-06-18 12:01:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require("path");
 
 // ✅ Allow both local and production frontends
 const allowedOrigins = [
-  "http://localhost:3000",
+  "http://localhost:3000","https://vastu-sanjivani-frontend.vercel.app/",
   
 ];
 
